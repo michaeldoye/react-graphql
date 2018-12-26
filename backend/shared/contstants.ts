0723fd95ef420856c42517da7439748bd7123b48
Michael Doye <mdoye@mobiquityinc.com>
2018-12-26 12:05:11 +0100

    refactor(app): added translate

--- backend/shared/contstants.ts
+++
@@ -1,4 +0,0 @@
-export const NEWS_ENDPOINT_XML = "https://magic.wizards.com/en/rss/rss.xml";
-export const TRANSLATE_ENDPOINT_JSON =
-    "https://translation.googleapis.com/language/translate/v2";
-export const TRANSLATE_API_KEY = "AIzaSyC67yKTuwHRQmxXHwh2j_EjtHqUNYpeL1g";
